6c471808fd7fad46d17a00e3304dbf027fa54a4d
Pedro Pavanello <pavanellopedro109@gmail.com>
2026-04-24 21:50:11 -0300

    chore: commit inicial do projeto e primeiras configurações

---
+++ src/test/java/com/devpavanello/park_api/ParkApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.devpavanello.park_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
