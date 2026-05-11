1212999faedf0f1b020c4f6dd668f03d8b41f531
Pedro Pavanello <pavanellopedro109@gmail.com>
2026-05-11 10:57:06 -0300

    feat(usuario): implementa endpoint de criação de usuário

--- src/main/java/com/devpavanello/park_api/service/UsuarioService.java
+++ src/main/java/com/devpavanello/park_api/service/UsuarioService.java
@@ -1,12 +1,19 @@
 package com.devpavanello.park_api.service;
 
+import com.devpavanello.park_api.entity.Usuario;
 import com.devpavanello.park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
