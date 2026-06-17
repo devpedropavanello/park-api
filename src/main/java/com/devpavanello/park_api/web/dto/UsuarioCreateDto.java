0508b3d8d8d55e7666e3e0c1b37fa6cf3db580d0
Pedro Pavanello <pavanellopedro109@gmail.com>
2026-06-17 11:41:37 -0300

    feat(usuario): adiciona validação no cadastro

--- src/main/java/com/devpavanello/park_api/web/dto/UsuarioCreateDto.java
+++ src/main/java/com/devpavanello/park_api/web/dto/UsuarioCreateDto.java
@@ -1,5 +1,8 @@
 package com.devpavanello.park_api.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,6 +12,10 @@ import lombok.*;
 @AllArgsConstructor
 public class UsuarioCreateDto {
 
+    @NotBlank
+    @Email(message = "formato do e-mail está inválido", regexp = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
     private String username;
+    @NotBlank
+    @Size(min = 6, max = 6, message = "a senha deve ter exatamente 6 caracteres")
     private String password;
 }
